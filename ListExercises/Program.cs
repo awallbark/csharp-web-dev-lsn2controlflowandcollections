8d501dee7fd52900aa725446c6a406d5a6235966
awallbark <awallbark@gmail.com>
2021-11-10 16:53:38 -0600

    Updates integers in list to meet exercise requirements

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -7,7 +7,7 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7 };
+            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
             Console.WriteLine(SumEvenNumbers(numbers));
         }
