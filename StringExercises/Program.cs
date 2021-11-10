f86b5b0166a3e8aaab07eb2ce94062c59a9508f1
awallbark <awallbark@gmail.com>
2021-11-09 20:17:06 -0600

    Adds String Method exercises

--- StringExercises/Program.cs
+++ StringExercises/Program.cs
@@ -6,7 +6,18 @@ namespace StringExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Strings!");
+            //string sentence = "I would not, could not, in a box. I would not, could not with a fox. " +
+            //"I will not eat them in a house. I will not eat them with a mouse.";
+
+            //string[] newSentence = sentence.Split(' ');
+
+            //Console.WriteLine(string.Join(",", newSentence));
+
+            string sentence2 = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
+
+            string[] newSentence2 = sentence2.Split(".");
+
+            Console.WriteLine(string.Join($".\n ", newSentence2));
         }
     }
 }
