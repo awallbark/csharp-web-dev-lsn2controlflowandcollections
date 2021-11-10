8ed2647334c66c57c1178933ee07243e4db7f362
awallbark <awallbark@gmail.com>
2021-11-09 19:42:14 -0600

    Adds Array to Array Exercises. Loops through values.

--- ArrayExercises/Program.cs
+++ ArrayExercises/Program.cs
@@ -6,7 +6,26 @@ namespace ArrayExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Arrays!");
+            int i;
+            
+            int[] someArray = new int[] { 1, 1, 2, 3, 5, 8 };
+
+            //printing whole list
+            foreach (var number in someArray)
+            {
+                Console.WriteLine(number);
+            }
+
+            Console.WriteLine();
+            //printing only odd numbers
+            for (i = 0; i < someArray.Length; i++)
+            {
+                //condition to check ODD number
+                //https://www.includehelp.com/dot-net/print-all-odd-numbers-from-array-of-integers-using-c-sharp-program.aspx
+                if (someArray[i] % 2 != 0)
+                    Console.Write(someArray[i] + " ");
+            }
+            
         }
     }
 }
