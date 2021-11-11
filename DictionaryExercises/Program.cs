d5defc9e15c48c830d74a70f7478a8619ec8dc93
awallbark <awallbark@gmail.com>
2021-11-11 17:11:19 -0600

    Creates Dictionary for StudentIDs and names.

--- DictionaryExercises/Program.cs
+++ DictionaryExercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace DictionaryExercises
 {
@@ -6,7 +7,37 @@ namespace DictionaryExercises
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<int, string> students = new Dictionary<int, string>();
+            int studentId;
+
+            Console.WriteLine($"Please enter the student ID number.\n Enter 0 to exit");
+
+            do
+            {
+                //collect the student ID
+                Console.WriteLine("StudentID: ");
+                studentId = Convert.ToInt32(Console.ReadLine());
+
+                if (studentId != 0)
+                {
+                    //collect student name and add the two to the Dictionary
+                    Console.WriteLine("Please enter the student name.");
+                    string studentName = Console.ReadLine();
+                    students.Add(studentId, studentName);
+
+                }
+
+            } while (studentId > 0);
+
+            Console.WriteLine("\nClass roster:");
+
+            foreach (KeyValuePair<int, string> student in students)
+            {
+                Console.WriteLine(student.Key + " (" + student.Value + ")");
+            }
+
+
+
         }
     }
 }
