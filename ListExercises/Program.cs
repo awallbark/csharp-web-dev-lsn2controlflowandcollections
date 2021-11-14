30b5c871f1d6e9f6014a3baf808f51bbdeea92bc
awallbark <awallbark@gmail.com>
2021-11-14 16:14:25 -0600

    Takes a string and puts it into a list.

--- ListExercises/Program.cs
+++ ListExercises/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ListExercises
 {
@@ -7,14 +8,25 @@ namespace ListExercises
     {
         static void Main(string[] args)
         {
-            List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            //List<int> numbers = new List<int>() { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
-            Console.WriteLine(SumEvenNumbers(numbers));
+            //Console.WriteLine(SumEvenNumbers(numbers));
 
-            List<string> fiveLetters = new List<string>() { "cat", "dog", "house", "dance", "sleep", "bed", "car" };
+            //List<string> fiveLetters = new List<string>() { "cat", "dog", "house", "dance", "sleep", "bed", "car" };
 
-            PrintFiveLetterWords(fiveLetters);
-            
+            //PrintFiveLetterWords(fiveLetters);
+
+
+            //Bonus Mission 3.75 Take the string from the split practice, move it into a list
+            string split = "I would not, could not, in a box. I would not, could not with a fox. " +
+            "I will not eat them in a house. I will not eat them with a mouse.";
+            List<string> list = new List<string>();
+            list = split.Split('.').ToList();
+            foreach (var l in list)
+            {
+                Console.WriteLine(l);
+            }
+
         }
 
         public static int SumEvenNumbers(List<int> numbers)
